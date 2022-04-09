443ef198ebc27b15f5957d178cf5b2dbd975269d
Jordan Parra <jordanparra15@gmail.com>
2022-04-08 22:29:21 -0500

    fixed the slideshow

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ async function loadByBreed(breed) {
     if (breed != "Choose a dog breed") {
         const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
         const data = await response.json()
-        console.log(data)
         createSlideshow(data.message)
     }
 }
@@ -43,17 +42,17 @@ function createSlideshow(images) {
 
     if (images.length > 1) {
         document.getElementById("slideshow").innerHTML = `
-    <div class="slide" style="background-image: url('${images[0]}')"></div>
-    <div class="slide" style="background-image: url('${images[1]}')"></div>
-    `
+        <div class="slide" style="background-image: url('${images[0]}')"></div>
+        <div class="slide" style="background-image: url('${images[1]}')"></div>
+        `
         currentPosition += 2
         if (images.length == 2) currentPosition = 0
-        timer = setInterval(nextSlide, 3000)
+        timer = setInterval(nextSlide, 5000)
     } else {
         document.getElementById("slideshow").innerHTML = `
-    <div class="slide" style="background-image: url('${images[0]}')"></div>
-    <div class="slide"></div>
-    `
+        <div class="slide" style="background-image: url('${images[0]}')"></div>
+        <div class="slide"></div>
+        `
     }
 
 
