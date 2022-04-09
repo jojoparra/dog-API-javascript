f8dbc96c5336f1c26f8f65f48358f5f4e3e0643a
Jordan Parra <jordanparra15@gmail.com>
2022-04-08 20:45:23 -0500

    added css

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+let timer
+let deleteFirstPhotoDelay
 //this is to call the inital api 
 async function start() {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all")
-    const data = await response.json()
-    createBreedList(data.message)
+    try {
+        const response = await fetch("https://dog.ceo/api/breeds/list/all")
+        const data = await response.json()
+        createBreedList(data.message)
+    } catch (e) {
+        console.log("There was a problem fetching the breed list")
+    }
 }
 
 start()
@@ -29,8 +35,37 @@ async function loadByBreed(breed) {
     }
 }
 
-// create the html for empty slideshpe div
+// create the html for empty slideshow div
 function createSlideshow(images) {
-    document.getElementById("slideshow").innerHTML = `<div class="slide"
-    style="background-image: url('${images[0]}')"></div>`
+    let currentPosition = 0
+    clearInterval(timer)
+    clearTimeout(deleteFirstPhotoDelay)
+
+    if (images.length > 1) {
+        document.getElementById("slideshow").innerHTML = `
+    <div class="slide" style="background-image: url('${images[0]}')"></div>
+    <div class="slide" style="background-image: url('${images[1]}')"></div>
+    `
+        currentPosition += 2
+        if (images.length == 2) currentPosition = 0
+        timer = setInterval(nextSlide, 3000)
+    } else {
+        document.getElementById("slideshow").innerHTML = `
+    <div class="slide" style="background-image: url('${images[0]}')"></div>
+    <div class="slide"></div>
+    `
+    }
+
+
+    function nextSlide() {
+        document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+        deleteFirstPhotoDelay = setTimeout(function () {
+            document.querySelector(".slide").remove()
+        }, 1000)
+        if (currentPosition + 1 >= images.length) {
+            currentPosition = 0
+        } else {
+            currentPosition++
+        }
+    }
 }
